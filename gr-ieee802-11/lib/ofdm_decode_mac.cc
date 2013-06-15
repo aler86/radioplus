fd03c571555d5892340330f9529c4ae75d681744
shashankgaur <shashankgaur@ieee.org>
2013-06-15 10:32:23 +0200

    Update Comments

--- gr-ieee802-11/lib/ofdm_decode_mac.cc
+++ gr-ieee802-11/lib/ofdm_decode_mac.cc
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, visit <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <gnuradio/ieee802_11/ofdm_decode_mac.h>
 #include <gnuradio/io_signature.h>
