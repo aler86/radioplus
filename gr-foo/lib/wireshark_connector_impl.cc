262e3168252a3013d9ed13374c286a8be24a995e
shashankgaur <shashankgaur@ieee.org>
2013-09-14 23:51:47 +0200

    Changes

--- gr-foo/lib/wireshark_connector_impl.cc
+++ gr-foo/lib/wireshark_connector_impl.cc
@@ -1,4 +1,4 @@
-/*
+/* For Master Branch
  * Copyright (C) 2013 Bastian Bloessl <bloessl@ccs-labs.org>, Appended by Shashank Gaur <shashankgaur@ieee.org>
  *
  * This program is free software: you can redistribute it and/or modify
